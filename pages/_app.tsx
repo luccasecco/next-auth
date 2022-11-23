98c16228e2726d085a14b5fb117f3123b2ae4083
luccasecco <luccasecco@hotmail.com>
2022-11-23 20:33:01 -0300

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { AuthProvider } from '../context/AuthContetx'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>
+}
