79fea46b7254c9e7d05641039523cab8775fe74a
luccasecco <luccasecco@hotmail.com>
2022-11-28 20:10:50 -0300

    mrp: continue

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,15 @@
+import { rejects } from 'assert';
 import axios, {AxiosError} from 'axios'
 import { parseCookies, setCookie } from 'nookies'
+import { resolve } from 'path';
 
 interface AxiosErrorResponse {
   code?: string;
 }
 
 let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue = []
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -22,24 +26,40 @@ api.interceptors.response.use(response => {
       cookies = parseCookies()
 
       const { 'nextauth.refreshToken': refreshToken } = cookies
+      const originalConfig = error.config
 
-      api.post('/refresh', {
-        refreshToken
-      }).then(response => {
-        const {token} = response.data
-
-        setCookie(undefined, 'nextauth.token', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days
-          path: '/'
-        })
+      if(!isRefreshing){
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const {token} = response.data
   
-        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days
-          path: '/'
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+    
+          setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
         })
+      }
 
-        api.defaults.headers['Authorization'] = `Bearer ${token}`
-      })
+      return new Promise(resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig?.headers?['Authorization'] = `Bearer ${token}`
+
+            resolve(api(originalConfig))
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err)
+          }
+        })
+      }
   } else {
 
   }
